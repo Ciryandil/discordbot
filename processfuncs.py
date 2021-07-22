647e71803216bbd9af5f1d3ca8d9cd83bb764650
LawlietRivers <legolas@live.in>
2021-07-22 21:45:30 +0530

    completed processfuncs

--- processfuncs.py
+++ processfuncs.py
@@ -11,6 +11,7 @@ def process_text(text):
 
 def get_tokens(text):
     import pickle
+    from keras.preprocessing.sequence import pad_sequences
     
     text_list=[text]
 
@@ -18,6 +19,26 @@ def get_tokens(text):
         tokenizer = pickle.load(handle)
     
     tokenized_text_list = tokenizer.texts_to_sequences(text_list)
-
+    tokenized_text_list = pad_sequences(tokenized_text_list, maxlen = 200)
     return tokenized_text_list
 
+def get_probs(text):
+
+    from keras import models 
+    text = process_text(text)
+    token_list = get_tokens(text)
+
+    comment_model = models.load_model('comment_class_model.h5')
+    probs = comment_model.predict(token_list)
+    probs = probs.flatten()
+    probs = probs.tolist()
+    labels = ['toxic', 'severely toxic', 'obscene', 'threat', 'insult', 'identity hate']
+    prob_dictionary = dict(zip(labels,probs)) 
+    
+    return prob_dictionary
+
+
+sentence = input('give text: ')
+print(process_text(sentence))
+print(get_tokens(sentence))
+print(get_probs(sentence))
